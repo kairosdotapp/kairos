bb19634c34f53da81d208ce467133261c5e7a597
Cliff Brake <cbrake@bec-systems.com>
2023-02-11 19:40:41 -0500

    parsing working

--- parser.go
+++ parser.go
@@ -4,16 +4,61 @@ import (
 	"bufio"
 	"fmt"
 	"regexp"
+	"strconv"
 )
 
 type entry struct {
-	date string
-	who  string
-	logs []string
-	time float32
+	date    string
+	account string
+	logs    []string
+	hours   float32
+}
+
+func (e *entry) setDate(d string) {
+	e.date = d
+	e.account = ""
+	e.logs = []string{}
+	e.hours = 0
+}
+
+func (e *entry) setAccount(a string) {
+	e.account = a
+	e.logs = []string{}
+	e.hours = 0
+}
+
+func (e *entry) hasDate() bool {
+	if e.date != "" {
+		return true
+	}
+
+	return false
+}
+
+func (e *entry) hasAccount() bool {
+	if e.account != "" {
+		return true
+	}
+
+	return false
+}
+
+func (e *entry) clearDate() {
+	e.date = ""
+	e.account = ""
+	e.logs = []string{}
+	e.hours = 0
+}
+
+func (e *entry) clearAccount() {
+	e.account = ""
+	e.logs = []string{}
+	e.hours = 0
 }
 
 var reDate = regexp.MustCompile(`^\d{4}-\d{2}-\d{2}`)
+var reAccount = regexp.MustCompile(`^(time\S*)\s*(([0-9]*[.])?[0-9]+)`)
+var reLog = regexp.MustCompile(`^  #\s*(\S+.*)`)
 
 type parserState int
 
@@ -23,9 +68,9 @@ const (
 )
 
 type parser struct {
-	scanner *bufio.Scanner
-	state   parserState
-	currentEntry *entry
+	scanner      *bufio.Scanner
+	state        parserState
+	currentEntry entry
 }
 
 func newParser(scanner *bufio.Scanner) *parser {
@@ -35,24 +80,50 @@ func newParser(scanner *bufio.Scanner) *parser {
 func (p *parser) scan() (*entry, error) {
 	for p.scanner.Scan() {
 		t := p.scanner.Text()
-		d := reDate.FindString(t)
 
+		d := reDate.FindString(t)
 		if len(d) > 0 {
 			// we found a new entry, do we have a current one?
-			if p.currentEntry != nil {
-				r := *p.currentEntry
-				p.currentEntry = &entry{date:d} 
-				fmt.Println("returning: ", r)
-				return &r, nil
+			ret := p.currentEntry
+			p.currentEntry.setDate(d)
+
+			if ret.hasDate() {
+				return &ret, nil
+			}
+			continue
+		}
+
+		aMatches := reAccount.FindStringSubmatch(t)
+		if len(aMatches) >= 2 {
+			a := aMatches[1]
+			ret := p.currentEntry
+			p.currentEntry.setAccount(a)
+
+			if len(aMatches) >= 3 {
+				hS := aMatches[2]
+				h, err := strconv.ParseFloat(hS, 32)
+				if err != nil {
+					return nil, fmt.Errorf("Error parsing hours: %v", err)
+				}
+				p.currentEntry.hours = float32(h)
 			}
 
-			p.currentEntry = &entry{date:d} 
+			if ret.hasAccount() {
+				return &ret, nil
+			}
+
+			continue
+		}
+
+		lMatches := reLog.FindStringSubmatch(t)
+		if len(lMatches) >= 2 {
+			p.currentEntry.logs = append(p.currentEntry.logs, lMatches[1])
 		}
 	}
 
-	if p.currentEntry != nil {
-		r := *p.currentEntry
-		p.currentEntry = nil
+	if p.currentEntry.hasDate() {
+		r := p.currentEntry
+		p.currentEntry.clearDate()
 		return &r, nil
 	}
 
--- parser_test.go
+++ parser_test.go
@@ -2,33 +2,81 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/kr/pretty"
 )
 
 var testData = `
 2023-01-17 # Tues
 time:cust:a              2
-  # weekly priority meeting
-  # work on updated rule offline design
-  # portal: update UI to create offline comp IO conditions
+  # meeting
+  # work on updated rule
 time:bec:admin           5
 time:cust:c              0.75
-  # work on debugging OE build issues
+  # debug build issues
 
 2023-01-18 # Wed
 time:cust:b              3
   # weekly design meeting
-  # work on system/app metrics
-  # work on USB performance
+  # work on metrics
+  # USB performance
 time:cust:c              0.5
-  # project setup, work on OE build issues
+  # project setup
 time:bec:siot:go         2
 time:bec:admin           3.25
 `
 
 func TestParser(t *testing.T) {
 	s := strings.NewReader(testData)
+
+	exp := []entry{
+		{
+			date:    "2023-01-17",
+			account: "time:cust:a",
+			logs:    []string{"meeting", "work on updated rule"},
+			hours:   2,
+		},
+		{
+			date:    "2023-01-17",
+			account: "time:bec:admin",
+			logs:    []string{},
+			hours:   5,
+		},
+		{
+			date:    "2023-01-17",
+			account: "time:cust:c",
+			logs:    []string{"debug build issues"},
+			hours:   0.75,
+		},
+		{
+			date:    "2023-01-18",
+			account: "time:cust:b",
+			logs:    []string{"weekly design meeting", "work on metrics", "USB performance"},
+			hours:   3,
+		},
+		{
+			date:    "2023-01-18",
+			account: "time:cust:c",
+			logs:    []string{"project setup"},
+			hours:   0.5,
+		},
+		{
+			date:    "2023-01-18",
+			account: "time:bec:siot:go",
+			logs:    []string{},
+			hours:   2,
+		},
+		{
+			date:    "2023-01-18",
+			account: "time:bec:admin",
+			logs:    []string{},
+			hours:   3.25,
+		},
+	}
 
 	scanner := bufio.NewScanner(s)
 
@@ -50,8 +98,19 @@ func TestParser(t *testing.T) {
 		entries = append(entries, *e)
 	}
 
-	if len(entries) != 2 {
-		t.Fatal("Expected 2 entries, got: ", len(entries))
+	if len(entries) != len(exp) {
+		t.Fatalf("Did not get the correct # of entries, exp %v, got %v", len(exp), len(entries))
+	}
+
+	if !reflect.DeepEqual(exp, entries) {
+		for i := range exp {
+			if !reflect.DeepEqual(exp[i], entries[i]) {
+				fmt.Println("Failed at index: ", i)
+				pretty.Println("exp: ", exp[i])
+				pretty.Println("entries: ", entries[i])
+			}
+		}
+		t.Fatal("did not get expected result")
 	}
 
 }
