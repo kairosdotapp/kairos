e90d5f13c6ebe83138cf3d547ba165654d91d700
Cliff Brake <cbrake@bec-systems.com>
2023-02-11 22:46:32 -0500

    implement rate find function

--- rate.go
+++ rate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/gocarina/gocsv"
 )
@@ -13,8 +14,59 @@ type rate struct {
 	Rate    float32 `csv:"rate"`
 }
 
-func parseRates(r io.Reader) ([]rate, error) {
-	var rates []rate
+// match scores how well an account and user matches
+// the rate. The user is heavily weighted:
+//   - user match: +100
+//   - rate user not set: 0
+//   - user's don't match: return immediately with score of 0
+//
+// the account score is based on the number of characters
+// in the rate account that match the query account param.
+// For instance, if the rate account is cust:a, and the query
+// account is cust:a:proj1, then we match 6 charcters and add
+// 6 to the score. If we find a rate later of cust:a:proj1, then
+// that would add 12 to the score.
+func (r *rate) match(account string, user string) int {
+	var score int
+
+	if r.User != "" {
+		if r.User == user {
+			score += 100
+		} else {
+			return 0
+		}
+	}
+
+	if strings.HasPrefix(account, r.Account) {
+		score += len(r.Account)
+	}
+
+	return score
+}
+
+type rates []rate
+
+func (r *rates) find(account string, user string) (float32, bool) {
+	var ret rate
+	var score int
+
+	for _, rt := range *r {
+		s := rt.match(account, user)
+		if s > score {
+			ret = rt
+			score = s
+		}
+	}
+
+	if score <= 0 {
+		return 0, false
+	}
+
+	return ret.Rate, true
+}
+
+func parseRates(r io.Reader) (rates, error) {
+	var rates rates
 
 	if err := gocsv.Unmarshal(r, &rates); err != nil {
 		return rates, fmt.Errorf("csv error: %v", err)
--- rate_test.go
+++ rate_test.go
@@ -13,15 +13,17 @@ account,user,rate
 time:cust:a,,80
 time:cust:a,fred,90
 time:cust:a:onsite,,150
+time:cust:a:onsite,fred,200
 time:cust:b,,70
 time:cust:c,,65
 `
 
 func TestRateParser(t *testing.T) {
-	exp := []rate{
+	exp := rates{
 		{"time:cust:a", "", 80},
 		{"time:cust:a", "fred", 90},
 		{"time:cust:a:onsite", "", 150},
+		{"time:cust:a:onsite", "fred", 200},
 		{"time:cust:b", "", 70},
 		{"time:cust:c", "", 65},
 	}
@@ -33,6 +35,40 @@ func TestRateParser(t *testing.T) {
 	if !reflect.DeepEqual(rates, exp) {
 		pretty.Println("exp: ", exp)
 		pretty.Println("rates: ", rates)
-		t.Fatal("Did not get expected rates: ", err)
+		t.Fatal("Did not get expected rates")
+	}
+}
+
+func TestRateFind(t *testing.T) {
+	tests := []struct {
+		account string
+		user    string
+		exp     float32
+	}{
+		{"time:cust:a", "", 80},
+		{"time:cust:a", "fred", 90},
+		{"time:cust:a:onsite", "", 150},
+		{"time:cust:b", "", 70},
+		{"time:cust:c", "cliff", 65},
+		{"time:cust:a:onsite", "fred", 200},
+	}
+
+	rates, err := parseRates(strings.NewReader(testRateData))
+	if err != nil {
+		t.Fatal("Error parsing rates: ", err)
+	}
+
+	for _, test := range tests {
+		r, ok := rates.find(test.account, test.user)
+		if !ok {
+			pretty.Printf("did not find expected rate for %v\n", test)
+			t.Fatal()
+		}
+
+		if r != test.exp {
+			pretty.Printf("rate for %v did not match, got: %v\n",
+				test, r)
+			t.Fatal()
+		}
 	}
 }
