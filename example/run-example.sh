f9629c11507eddb8a43d376aaf7329adcdd5a51d
Cliff Brake <cbrake@bec-systems.com>
2024-05-01 12:05:28 -0400

    add note in example

--- example/run-example.sh
+++ example/run-example.sh
@@ -2,3 +2,6 @@
 
 go run ../ invoice -account time:cust:abcorp -num 1002 -month 2024-04 -template ../invoice.tpl
 go run ../ invoice -account time:cust:xyzinc -num 1003 -month 2024-04 -template ../invoice.tpl
+
+# Note, instead of "go run ../", you can download the latest release and run kairos
+# from the command line.
